2187f704781279829cb2773b0347e13c06d73529
Ashutosh <ashu.cse.sln@gmail.com>
2022-08-25 18:54:10 +0530

    List of available operation are

---
+++ arithmatic.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p "Enter the First Value " a;
+read -p "Enter the Second Value " b;
+read -p "Enter the Third Value " c;
+echo $a $b $c;
+echo "Available operation are ....";
+echo "1.a+b*c";
+echo "2.c+a/b";
+echo "3.a%b+c";
+echo "4.a*b+c";
