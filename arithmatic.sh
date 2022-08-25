24ac37e385218727658068e1f0dc52644186cfdd
Ashutosh <ashu.cse.sln@gmail.com>
2022-08-25 18:59:01 +0530

    code completed

--- arithmatic.sh
+++ arithmatic.sh
@@ -8,3 +8,21 @@ echo "1.a+b*c";
 echo "2.c+a/b";
 echo "3.a%b+c";
 echo "4.a*b+c";
+read -p "enter Choice : " ch;
+if [ $ch -eq 1 ]
+then
+	temp=$((b*c));
+	result=$((temp+a));
+elif [ $ch -eq 2 ]
+then
+	temp=$((a/b));
+	result=$((temp+c));
+elif [ $ch -eq 3 ]
+then
+	temp=$((a%b));
+	result=$((temp+c));
+else
+	temp=$((a*b));
+	result=$((temp+c));
+fi
+echo $result;
